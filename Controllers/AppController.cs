b5a2168a2d0f10aa005fdfe1e0f235a512eb79aa
Poorna Oruganti <poorna.oruganti@STLERI.COM>
2023-03-15 13:51:33 +0530

    frist commit

---
+++ Controllers/AppController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebApplication1.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
